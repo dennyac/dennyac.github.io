be8fcb11ae696224eb77b21d3b2df4416d0b3a2b
Denny Abraham Cheriyan <dennyac@gmail.com>
2015-05-09 18:00:37 -0700

    Changed contact form to Formspree, added google analytics and disqus comments

--- js/denny-blog.js
+++ js/denny-blog.js
@@ -1,3 +1,5 @@
+---
+---
 /*!
  * Clean Blog v1.0.0 (http://startbootstrap.com)
  * Copyright 2015 Start Bootstrap
@@ -23,7 +25,7 @@ $(function() {
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
+            // var phone = $("input#phone").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
@@ -31,11 +33,10 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "//formspree.io/{{ site.email }}",
                 type: "POST",
                 data: {
                     name: name,
-                    phone: phone,
                     email: email,
                     message: message
                 },
